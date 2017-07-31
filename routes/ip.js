2f7c742680bc30397a25167834d5eb4b6ac4dd63
root <wangxiang@xiangblog.com>
2017-07-31 16:01:38 +0800

    update script

--- routes/ip.js
+++ routes/ip.js
@@ -6,13 +6,16 @@ const $ = require('cheerio');
 router.get('/:ip', function(req, res, next) {
     let ip = req.params.ip;
     if (ip == 'me') {
-        ip = req.ip;
-        if (ip == '::1') {
+        let req_ip = req.ip;
+        if (req_ip == '::1') {
             ip = '127.0.0.1';
+        }
+        if (req_ip.indexOf('::ffff:') >=0 ){
+            ip = req_ip.substring(7);
         }
         let xip = req.headers['x-forwarded-for'];
         if (xip) {
-            ip = xip.split(',')[0] || req.ip;
+            ip = xip.split(',')[0] || ip;
         }
     } else {
         let matcher = /^(?:(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)\.){3}(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)$/;
